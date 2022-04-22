50de0c5f5ba543f35474759d675474a98b001128
jfan0010 <48777884+jfan0010@users.noreply.github.com>
2022-04-23 09:33:36 +1000

    added title for card list

--- client/src/components/shared/JourneyCardList.js
+++ client/src/components/shared/JourneyCardList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import JourneyCard from "./JourneyCard";
 
-const JourneyCardList = ({ journeyList, savedList }) => {
+const JourneyCardList = ({ journeyList, savedList, listTitle }) => {
   return (
     <>
+      <div style={{ marginBottom: "5px", marginLeft: "5px" }}>{listTitle}</div>
       {journeyList.map((journey, index) => (
         <JourneyCard key={index} journey={journey} savedList={savedList} />
       ))}
