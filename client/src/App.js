18bb3b02531e90d37d67571cad1cced280ebe0b9
yjii0013@student.monash.edu <yjii0013@student.monash.edu>
2022-04-23 14:22:51 +1000

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,17 @@
-
-import React from "react";
-import Homepage from "./components/Homepage";
 import Form from "./components/Formpage/Form";
 import ProgressBar from "./components/Journeypage/ProgressBar";
 import { ChakraProvider } from "@chakra-ui/react";
-import styles from '../src/styles/styles.css'
-import SavedJourneyPage from "./components/SavedJourneyPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-import LoginModal from "./components/shared/Login";
-import SignupModal from "./components/shared/Signup";
+import React, { useState } from "react";
+import Homepage from "./components/Homepage";
+import SavedJourneyPage from "./components/SavedJourneyPage";
+import journeyList from "./dummyJourney.json";
+import userData from "./dummyUser.json";
 
 function App() {
-  const [data, setData] = React.useState([
-    {
-      id: 1,
-      title: "Day 1",
-      content:
-        "Python is a programming language that is used to create programs that run on a computer. It is a very popular language for beginners.",
-      isCompleted: true,
-    },
-    {
-      id: 2,
-      title: "Day 2",
-      content:
-        "Python is a programming language that is used to create programs that run on a computer. It is a very popular language for beginners.",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Day 3",
-      content:
-        "Python is a programming language that is used to create programs that run on a computer. It is a very popular language for beginners.",
-      isCompleted: false,
-    },
-    {
-      id: 4,
-      title: "Day 4",
-      content:
-        "Python is a programming language that is used to create programs that run on a computer. It is a very popular language for beginners.",
-      isCompleted: false,
-    },
-  ]);
+  // Global variables passing around
+  const [isLoggedIn, setLogged] = useState(true);
+  const [username, setUsername] = useState(userData.username);
 
   return (
     <div>
