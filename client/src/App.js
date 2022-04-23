f336036cfec725d7b899bbea4d1cdeac1c7a741d
jfan0010 <48777884+jfan0010@users.noreply.github.com>
2022-04-23 10:04:26 +1000

    Display saved journey

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-import Homepage from "./components/Homepage";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Homepage from "./components/Homepage";
+import LoggedSavedJourneyPage from "./components/LoggedSavedJourneyPage";
+import journeyList from "./dummyJourney.json";
+import userData from "./dummyUser.json";
 
 function App() {
-  return <Homepage />;
+  return (
+    <LoggedSavedJourneyPage
+      username={userData.username}
+      journeyList={journeyList}
+      savedList={userData.saved_journey}
+    />
+  );
 }
 
 export default App;
